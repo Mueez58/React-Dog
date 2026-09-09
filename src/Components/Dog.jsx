a53b1c7379cc3931cc7a777c5e807b26bfd5b53d
Mueez58 <mueez7200@gmail.com>
2026-09-09 11:28:52 +0500

    react-dog

--- src/Components/Dog.jsx
+++ src/Components/Dog.jsx
@@ -63,7 +63,7 @@ const Dog = () => {
   <>
   <primitive object={model.scene} position={[0.25,-0.55,0]}  rotation={[ 0, Math.PI / 3.9, 0]}/>
   <directionalLight position={[0,5,5]} color={0xFFFFFF} intensity={10} />
-
+  
   </>
   )
 }
